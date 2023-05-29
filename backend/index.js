3dfde3a4c4093f7238d9098feaf661f1937182ad
adilsoukaini <47067476+adilsoukaini@users.noreply.github.com>
2023-05-29 00:12:00 -0100

    the final chat app was easy it uses simple npm chat engines fro ui,chatengine.io asn api

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put('https://api.chatengine.io/users/',{username: username,secret:username, first_name:username},{headers:{"private-key":"5d90c444-14e4-40ed-a725-5b17c42b3f62"}})
+    return res.status(r.status).json(r.data)
+  }
+  catch(e) {
+    return res.status(e.response.status).json(e.response.data)
+  } 
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001); 
